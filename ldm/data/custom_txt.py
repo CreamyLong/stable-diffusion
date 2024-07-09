7344be6502daa4f62e288bf7184e94416c4029cb
CreamyLong <45339996+CreamyLong@users.noreply.github.com>
2024-07-09 20:41:56 +0800

    Update custom_txt.py

--- ldm/data/custom_txt.py
+++ ldm/data/custom_txt.py
@@ -13,6 +13,20 @@ import json
 import albumentations
 
 
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import PIL
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+import glob
+import time
+import cv2
+import json
+import albumentations
+
 class CustomBase(Dataset):
     def __init__(self, img_paths, txt_paths,  size=None, random_crop=False, random_flip=False, random_rotate=False):
 
@@ -37,6 +51,7 @@ class CustomBase(Dataset):
                 self.flipor = albumentations.HorizontalFlip(p=0.5)
             if self.random_rotate:
                 self.rotator = albumentations.RandomRotate90(p=0.5)
+            
             self.preprocessor = albumentations.Compose([self.rescaler, self.cropper, self.flipor, self.rotator])
         else:
             self.preprocessor = lambda **kwargs: kwargs
@@ -53,7 +68,7 @@ class CustomBase(Dataset):
 
 
     def __len__(self):
-        return len(self.data)
+        return len(self.img_paths) #我修改的
 
     def __getitem__(self, i):
         example = {}
@@ -62,10 +77,9 @@ class CustomBase(Dataset):
         example["caption"] = self.txt_paths[i]
         return example
 
-
 class CustomTrain(CustomBase):
-    def __init__(self, size, training_images_list_file="data/cap_sliced_train.txt", training_txt_list_file=None):
-        super().__init__()
+    def __init__(self, size, training_images_list_file="data/coco_images.txt", training_txt_list_file="data/coco_txt.txt"):
+       
         self.size = size
 
         with open(training_images_list_file, "r") as f:
@@ -74,10 +88,11 @@ class CustomTrain(CustomBase):
         with open(training_txt_list_file, "r") as f:
             self.txt_paths = f.read().splitlines()
 
+        super().__init__(img_paths=self.img_paths, txt_paths=self.txt_paths,size=self.size)
 
 class CustomTest(CustomBase):
-    def __init__(self, size, test_images_list_file="data/cap_sliced_train.txt", test_classes_list_file=None):
-        super().__init__()
+    def __init__(self, size, test_images_list_file="data/coco_images.txt", test_classes_list_file="data/coco_txt.txt"):
+        
         self.size = size
 
         with open(test_images_list_file, "r") as f:
@@ -85,6 +100,8 @@ class CustomTest(CustomBase):
 
         with open(test_classes_list_file, "r") as f:
             self.txt_paths = f.read().splitlines()
+
+        super().__init__(img_paths=self.img_paths, txt_paths=self.txt_paths,size=self.size)
 
 
 
